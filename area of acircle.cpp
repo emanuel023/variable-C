6f04b6bc34302a7d8ded3542c975d3e6cb28ae53
emanuel023 <85346950+emanuel023@users.noreply.github.com>
2021-06-11 15:56:37 +0300

    Add files via upload

---
+++ area of acircle.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	
+ float area,radius;
+ printf("enter the radius of circle: ");
+ scanf("%f",&radius);
+ area=3.142*radius*radius;
+ printf("Area of the circle %f", area);
+ 
+ return 0;
+ 
+}
